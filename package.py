62e9ad666d4727214321c9cf8a69f78fcc75269a
Dananjaya Ramanayake <dananjaya@cynetis-embedded.com>
2022-11-16 10:26:01 +0100

    created a release tag

--- package.py
+++ package.py
@@ -1,9 +1,11 @@
+# Generates a ZIP file
+
 import shutil
 import yaml
 
 zip_version = 0
 
-with open('info.yaml','r') as yaml_file:
+with open('info.yaml', 'r') as yaml_file:
     data_from_yaml = yaml.safe_load(yaml_file)
     zip_version = data_from_yaml['version']
 
