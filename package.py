e1da7e09cdfc0fb148d4349550421dd2e1a71eb3
Dananjaya Ramanayake <dananjaya@cynetis-embedded.com>
2022-11-16 10:14:51 +0100

    add changelog + GH actions plugin

--- package.py
+++ package.py
@@ -7,6 +7,7 @@ with open('info.yaml','r') as yaml_file:
     data_from_yaml = yaml.safe_load(yaml_file)
     zip_version = data_from_yaml['version']
 
-zip_file_name = "src_{}".format(zip_version.replace('.','_'))
+#zip_file_name = "src_v{}-{}".format(zip_version,'release')
+zip_file_name = "code_release"
 
 shutil.make_archive(zip_file_name, 'zip', 'src')
